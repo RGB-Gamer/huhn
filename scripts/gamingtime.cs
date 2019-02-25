5b5096a6845adbf83ccf122c958c46873a1ab46f
RGB-Gamer <47953647+RGB-Gamer@users.noreply.github.com>
2019-02-25 18:28:45 +0100

    Add files via upload

---
+++ scripts/gamingtime.cs
@@ -0,0 +1,64 @@
+﻿
+using UnityEngine;
+
+public class gamingtime : MonoBehaviour {
+
+    public Rigidbody gam;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    private bool jump;
+    private bool a;
+
+    /// <summary>
+    /// Gamingtime
+    /// </summary>
+
+    // Update is called once per frame
+
+    void OnCollisionEnter(Collision collisioninfo)
+    {
+
+        if (collisioninfo.collider.name == "Gamingtime")
+        {
+            a = true;
+        }
+    }
+
+
+    private void Update()
+    {
+      
+       if (Input.GetKey("w") & a)
+        {
+            jump = true;
+            a = false;
+        }
+
+
+       
+
+     
+    }
+
+
+    void FixedUpdate () {
+        gam.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            gam.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            gam.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (jump)
+        {
+          gam.AddForce(0, 1000, 0);
+            jump = false;
+        }
+    }
+}
---
+++ scripts/playercollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercollision : MonoBehaviour {
+
+
+    public gamingtime movement;
+
+	// Use this for initialization
+ void OnCollisionEnter (Collision collisioninfo)
+    {
+        if (collisioninfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+
+}
