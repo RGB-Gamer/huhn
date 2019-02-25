ad57112bf44753845238f412e1491e041d19bc80
SoerenKierk <47953520+SoerenKierk@users.noreply.github.com>
2019-02-25 18:11:05 +0000

    Update playermovement.cs

--- playermovement.cs
+++ playermovement.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour {
 
-    public Transform Player;
-    public Vector3 offset;
-
+    	public Transform Player;
+    	public Vector3 offset;
+	public Transform fluegell;
+	public Transform fluegelr;
+	public  var float upforce = 500f;
+	public  var float forward = 500f;
+	public  var float sideforce = 500f;
+	
+	
     void Update () {
+	    Debug.Log (fluegelr);
+	    /*
+	    if(fluegelr.y>0){
+	    gam.AddForce(forward,upforce ,  sideforce) //ForceMode.VelocityChange
+	    
+	    }
+	    if(fluegelr.y>0){
+	    gam.AddForce(forward,upforce , -sideforce)// ForceMode.VelocityChange
+	    
+	    }
+	    
+	    */
 
 
         transform.position = Player.position ; 
